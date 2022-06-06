8e5e091f6c8429d583ff391426623b367cfcb405
Alex Kuzmin <88483359+AleksKuzmin@users.noreply.github.com>
2022-06-06 12:52:44 +1000

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FloorplanComponent } from './floorplan/floorplan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FloorplanComponent
   ],
   imports: [
     BrowserModule
